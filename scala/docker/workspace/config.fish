4042be00ccb3135e0c4f3f47509d30e13e726565
poposuke1995x <jimi.woodstock1995@gmail.com>
2020-11-01 22:31:59 +0900

    scalaのDockerfileを修正

--- scala/docker/workspace/config.fish
+++ scala/docker/workspace/config.fish
@@ -11,3 +11,13 @@ set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
 #################################################
 
 alias sbt="/root/.sdkman/candidates/sbt/current/bin/sbt"
+
+################# 自動でls ######################
+functions --copy cd standard_cd
+function cd
+    standard_cd $argv; and ls
+end
+#################################################
+
+# ローカル開発環境のDB設定
+set -x JDBC_DATABASE_URL "jdbc:postgresql://postgres:5432/postgres?user=postgres&password=postgres"
